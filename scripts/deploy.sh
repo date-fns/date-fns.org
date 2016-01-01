821e17b10dde4182e41b169eb5ee4269182c062d
Sasha Koss <koss@nocorp.me>
2016-01-01 20:16:21 +0530

    Fix Firebase deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
+# Fail on non-zero exit code
+set -e
+
+echo '~ Fetching the latest date-fns'
 npm install date-fns@latest
+
+echo '~ Building the application'
 npm run build
+
+echo '~ Running Firebase deploy'
 cd dist
-../node_modules/.bin/firebase deploy --token "${FIREBASE_TOKEN}"
+../node_modules/.bin/firebase deploy --token "$FIREBASE_TOKEN"
+
+echo '+ Done!'
