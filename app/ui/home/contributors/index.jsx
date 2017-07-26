5e5c60fbc526476e2cf15bc1b8379690d7210b75
Sasha Koss <koss@nocorp.me>
2017-07-26 20:55:39 +0300

    Fix contributors link

--- app/ui/home/contributors/index.jsx
+++ app/ui/home/contributors/index.jsx
@@ -1,41 +1,55 @@
 import React from 'react'
-import HomeBlock, {Link} from '../_lib/block'
-import {fetchContributors} from 'app/acts/contributors'
+import HomeBlock, { Link } from '../_lib/block'
+import { fetchContributors } from 'app/acts/contributors'
 import ImmutablePropTypes from 'react-immutable-proptypes'
-import {EitherPropType} from 'app/types/either'
+import { EitherPropType } from 'app/types/either'
 
-export default function Contributors ({contributors}) {
-  return <HomeBlock
-    header='Contributors'
-    action='Contribute to date-fns'
-    actionHref='http://localhost:5000/docs/Contributing'
-  >
-    {renderContent(contributors)}
-  </HomeBlock>
-}
-
-Contributors.propTypes = {
-  contributors: EitherPropType(React.PropTypes.object, ImmutablePropTypes.list.isRequired).isRequired
-}
-
-function renderContent (contributors) {
-  return contributors.fold(
-    ({message}) => message,
-    renderList
+export default function Contributors ({ contributors }) {
+  return (
+    <HomeBlock
+      header='Contributors'
+      action='Contribute to date-fns'
+      actionLink='doc'
+      actionLinkParams={{ docId: 'Contributing' }}
+    >
+      {renderContent(contributors)}
+    </HomeBlock>
   )
 }
 
+Contributors.propTypes = {
+  contributors: EitherPropType(
+    React.PropTypes.object,
+    ImmutablePropTypes.list.isRequired
+  ).isRequired
+}
+
+function renderContent (contributors) {
+  return contributors.fold(({ message }) => message, renderList)
+}
+
 function renderList (contributors) {
-  return <ol className='contributors-list'>
-    {contributors.map(contributor => <li className='contributors-item' key={contributor.get('login')}>
-      <Link href={contributor.get('html_url')} title={contributor.get('contributions')}>
-        <span className='contributors-link_content'>
-          <img className='contributors-avatar' src={contributor.get('avatar_url')} alt={`@${contributor.get('login')}'s avatar`} />
-          <span className='contributors-name'>
-            @{contributor.get('login')}
-          </span>
-        </span>
-      </Link>
-    </li>)}
-  </ol>
+  return (
+    <ol className='contributors-list'>
+      {contributors.map(contributor =>
+        <li className='contributors-item' key={contributor.get('login')}>
+          <Link
+            href={contributor.get('html_url')}
+            title={contributor.get('contributions')}
+          >
+            <span className='contributors-link_content'>
+              <img
+                className='contributors-avatar'
+                src={contributor.get('avatar_url')}
+                alt={`@${contributor.get('login')}'s avatar`}
+              />
+              <span className='contributors-name'>
+                @{contributor.get('login')}
+              </span>
+            </span>
+          </Link>
+        </li>
+      )}
+    </ol>
+  )
 }
