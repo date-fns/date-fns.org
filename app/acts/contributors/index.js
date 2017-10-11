002dbfd32a8826021f5d847d471aee0aa0612347
Sasha Koss <koss@nocorp.me>
2017-10-11 20:24:38 +0800

    Fetch more than 30 contributors from GitHub API

--- app/acts/contributors/index.js
+++ app/acts/contributors/index.js
@@ -4,7 +4,7 @@ import Either from 'app/types/either'
 import { getJSON } from 'app/_lib/request'
 
 export function fetchContributors () {
-  return getJSON('https://api.github.com/repos/date-fns/date-fns/contributors')
+  return getJSON('https://api.github.com/repos/date-fns/date-fns/contributors?per_page=999')
     .then(contributors =>
       act(state =>
         state.set('contributors', Either.Right(I.fromJS(contributors)))
