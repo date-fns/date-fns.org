3e2b4a3079af00b701976e5b3f4937736f3af51a
Sasha Koss <koss@nocorp.me>
2020-05-06 16:19:49 +0300

    Update the sponsors list

--- app/ui/home/sponsorship/index.jsx
+++ app/ui/home/sponsorship/index.jsx
@@ -2,14 +2,35 @@ import React from 'react'
 import HomeBlock, { Link, Text } from '../_lib/block'
 import members from './members.json'
 
-const bronzeSponsors = members.filter(({ tier }) => tier === 'Bronze Sponsors')
-const backers = members.filter(({ tier }) => tier === 'Backers')
+const silverSponsors = members.filter(
+  ({ tier, isActive }) => isActive && tier === 'Silver Sponsors'
+)
+const bronzeSponsors = members.filter(
+  ({ tier, isActive }) => isActive && tier === 'Bronze Sponsors'
+)
+const backers = members.filter(
+  ({ tier, lastTransactionAmount, createdAt, isActive }) =>
+    isActive &&
+    tier === 'Backers' &&
+    lastTransactionAmount >= 10 &&
+    new Date(createdAt).getTime() < new Date(2020, 4, 5).getTime()
+)
 
 export default function Sponsorship() {
   return (
     <HomeBlock header="Sponsors">
+      <h2 className="sponsorship-subheader">Silver sponsors</h2>
+
       <div className="sponsorship-list">
-        {bronzeSponsors.map(sponsor => (
+        {silverSponsors.map((sponsor) => (
+          <Sponsor key={sponsor.MemberId} sponsor={sponsor} />
+        ))}
+      </div>
+
+      <h2 className="sponsorship-subheader">Bronze sponsors</h2>
+
+      <div className="sponsorship-list">
+        {bronzeSponsors.map((sponsor) => (
           <Sponsor key={sponsor.MemberId} sponsor={sponsor} />
         ))}
       </div>
@@ -17,8 +38,8 @@ export default function Sponsorship() {
       <h2 className="sponsorship-subheader">Backers</h2>
 
       <div className="sponsorship-list">
-        {backers.map(sponsor => (
-          <Sponsor key={sponsor.MemberId} sponsor={sponsor} />
+        {backers.map((sponsor) => (
+          <Sponsor key={sponsor.MemberId} sponsor={sponsor} compact />
         ))}
       </div>
 
@@ -27,37 +48,60 @@ export default function Sponsorship() {
         fame and support the developers?
       </Text>
 
-      <a
-        href="https://opencollective.com/date-fns"
-        className="sponsorship-cta"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Sponsor date-fns
-      </a>
+      <div className="sponsorship-actions">
+        <a
+          href="https://opencollective.com/date-fns"
+          className="sponsorship-cta"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Sponsor date-fns
+        </a>
+
+        <a
+          href="https://opencollective.com/date-fns#section-contributors"
+          className="sponsorship-contributors"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          See all contributors
+        </a>
+      </div>
     </HomeBlock>
   )
 }
 
-function Sponsor ({ sponsor }) {
-  return <Link
-    href={`${sponsor.website || sponsor.profile}?ref=date-fns`}
-    newTab={true}
-  >
-    <div className="sponsorship-item">
-      <div className={`sponsorship-item-image-container ${sponsor.tier === 'Bronze Sponsors' ? 'sponsorship-item-image-container-bronze' : ''}`}>
-        {sponsor.image
-          ? (
+function Sponsor({ sponsor, compact }) {
+  return (
+    <Link
+      href={`${sponsor.website || sponsor.profile}?ref=date-fns`}
+      newTab={true}
+    >
+      <div className="sponsorship-item">
+        <div
+          className={`sponsorship-item-image-container ${
+            sponsor.tier === 'Silver Sponsors'
+              ? 'sponsorship-item-image-container-silver'
+              : sponsor.tier === 'Bronze Sponsors'
+              ? 'sponsorship-item-image-container-bronze'
+              : ''
+          }`}
+        >
+          {sponsor.image ? (
             <img
               src={sponsor.image}
               alt={`${sponsor.name} logo`}
               className="sponsorship-item-image"
             />
-          ) : (<div className="sponsorship-item-image-default"/>)}
+          ) : (
+            <div className="sponsorship-item-image-default" />
+          )}
+        </div>
+
+        {!compact && (
+          <span className="sponsorship-item-name">{sponsor.name}</span>
+        )}
       </div>
-      <span className="sponsorship-item-name">
-        {sponsor.name}
-      </span>
-    </div>
-  </Link>
+    </Link>
+  )
 }
