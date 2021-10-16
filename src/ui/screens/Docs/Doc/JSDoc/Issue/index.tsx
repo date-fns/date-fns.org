6c1318c7c4c192f3712755fbf4c1855906d7929c
Tetiana <ttobin@protonmail.ch>
2021-10-16 17:35:24 +0100

    Add more issue items

--- src/ui/screens/Docs/Doc/JSDoc/Issue/index.tsx
+++ src/ui/screens/Docs/Doc/JSDoc/Issue/index.tsx
@@ -2,44 +2,53 @@ import { h } from 'preact'
 import { useContext } from 'preact/hooks'
 import { Link } from '~/ui/components/Home/style.css'
 import { RouterContext, RouterLink } from '~/ui/router'
-import templateText from './templateText'
+import { bugTemplateText, docIssueTemplateText } from './templateText'
 
 export default function Issue() {
   const { location } = useContext(RouterContext)
-  const pageParams = location.params || { version: '' }
-  let pageUrl = ''
-
-  if ('version' in pageParams) {
-    pageUrl = `https://date-fns.org/${pageParams.version}/docs/${location.params?.page}`
-  }
-
+  const pageUrl = window.location.href
   const fnName = location.params?.page || ''
-  const issueUrl = templateText(pageUrl, fnName)
+  const docIssueUrl = docIssueTemplateText(pageUrl, fnName)
+  const bugIssueUrl = bugTemplateText(pageUrl, fnName)
 
   return (
     <section>
       <h2 id="issue">Found an issue with this page?</h2>
 
-      <div>
-        <a href={issueUrl} target="_blank">
-          Report a bug in the function.
-        </a>
-      </div>
+      <ul>
+        <li>
+          <div>
+            <Link
+              decorated
+              tag={RouterLink}
+              to={{
+                name: 'docs',
+                params: {
+                  page: 'Contributing',
+                },
+              }}
+            >
+              Suggest edits by sending a PR
+            </Link>
+          </div>
+        </li>
 
-      <div>
-        <Link
-          decorated
-          tag={RouterLink}
-          to={{
-            name: 'docs',
-            params: {
-              page: 'Contributing',
-            },
-          }}
-        >
-          Suggest edits or report a problem in the documentation.
-        </Link>
-      </div>
+        <li>
+          <div>
+            <a href={docIssueUrl} target="_blank" rel="noreferrer noopener">
+              Open an issue with this documentation
+            </a>
+          </div>
+        </li>
+
+        <li>
+          <div>
+            <a href={bugIssueUrl} target="_blank" rel="noreferrer noopener">
+              Report a bug in the function
+            </a>
+          </div>
+        </li>
+      </ul>
     </section>
   )
 }
--- src/ui/screens/Docs/Doc/JSDoc/Issue/templateText.ts
+++ src/ui/screens/Docs/Doc/JSDoc/Issue/templateText.ts
@@ -1,19 +1,56 @@
-/**
- * Returns GitHub issue template URL with prefilled title and body
- */
-export default function templateText(url: string, fn: string): string {
-  const githubUrl =
-    'https://github.com/date-fns/date-fns/issues/new?assignees=&labels=&template=bug_report.md'
+const githubUrl =
+  'https://github.com/date-fns/date-fns/issues/new?assignees=&labels=&template=bug_report.md'
 
+/**
+ * Returns GitHub document issue template URL with prefilled title and body
+ */
+export function docIssueTemplateText(url: string, fn: string): string {
   const title = encodeURIComponent(`Problem in ${fn} documentation`)
   const body = encodeURIComponent(`
-    ## The problem 
+   There is an issue with the [${fn} documentation](${url})
 
-    <!-- Describe the problem in the documentation -->
+  ### The problem with isDate documentation
 
-    ## Suggested edits
+  <!-- Describe the problem in the documentation -->
 
-    <!-- Suggest how the documentation can be improved -->
+  ### Suggested edits
+
+  <!-- Suggest how the documentation can be improved -->
+  `)
+  return githubUrl + '&title=' + title + '&body=' + body
+}
+
+/**
+ * Returns GitHub function issue template URL with prefilled title and body
+ */
+export function bugTemplateText(url: string, fn: string): string {
+  const title = encodeURIComponent(`Problem with ${fn} function`)
+
+  const body = encodeURIComponent(`
+  There is an issue with the [${fn} function](${url})
+  ### Describe the bug
+
+ 
+  #### 💻 Code if available
+
+
+  #### 🙁  Actual behavior
+
+
+  #### 🙂  Expected behavior
+
+
+  #### 🎮  Playground link if available
+
+
+  #### Software used:
+  - Browser [e.g. chrome, safari]
+  
+  - date-fns version [e.g. 2.24]
+  
+  #### Additional info
+  <!--Add any other info about the problem here. -->
+
   `)
   return githubUrl + '&title=' + title + '&body=' + body
 }
