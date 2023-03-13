c099fceb2ddbfe09a22fd3096c2a6e857c518fa4
Sasha Koss <koss@nocorp.me>
2023-03-13 09:15:38 +0800

    Add CDN to usage

--- src/ui/components/DocUsage/index.tsx
+++ src/ui/components/DocUsage/index.tsx
@@ -1,17 +1,19 @@
-import { h, Fragment, FunctionComponent } from 'preact'
-import { useEffect, useState } from 'preact/hooks'
 import type { DateFnsDocs } from '@date-fns/docs/types'
-import { Content } from './Content'
-import { DocHeaderAnchor } from '~/ui/components/DocHeaderAnchor'
-import * as styles from './styles.css'
 import classNames from 'classnames'
+import { Fragment, FunctionComponent, h } from 'preact'
+import { useEffect, useState } from 'preact/hooks'
+import { Code } from '~/ui/components/Code'
+import { Markdown } from '~/ui/components/Markdown'
+import { Usage } from '~/utils/docs'
 import { SectionHeader } from '../SectionHeader'
+import * as styles from './styles.css'
 
 const LOCALSTORAGE_KEY = 'usageSource'
 const DEFAULT_SOURCE = 'commonjs'
+const LS_SELECTED_OPTIONS_KEY = 'usageSelectedOptions'
 
 interface Props {
-  usage: DateFnsDocs.FnUsage
+  usage: Usage
   usageTabs: string[]
   scope?: string
   header?: 'h2' | 'h3'
@@ -24,6 +26,19 @@ export const DocUsage: FunctionComponent<Props> = ({
   header,
 }) => {
   const [source, setSource] = useState(DEFAULT_SOURCE)
+  const [selectedOptions, setSelectedOptionsState] = useState<
+    Record<string, string>
+  >(getSelectedOptions())
+
+  function setSelectedOptions(newOptions: Record<string, string>) {
+    setSelectedOptionsState(newOptions)
+    try {
+      window.localStorage.setItem(
+        LS_SELECTED_OPTIONS_KEY,
+        JSON.stringify(newOptions)
+      )
+    } catch (_e) {}
+  }
 
   useEffect(() => {
     const localStorageSource = window.localStorage.getItem(LOCALSTORAGE_KEY)
@@ -38,6 +53,18 @@ export const DocUsage: FunctionComponent<Props> = ({
   const selectedTab = usageTabs.includes(source) ? source : usageTabs[0]
   const selectedUsage = usage[selectedTab]
 
+  useEffect(() => {
+    if (!('options' in selectedUsage)) return
+
+    if (!selectedOptions[selectedTab]) {
+      const firstOption = selectedUsage.options[0]
+      setSelectedOptions({
+        ...selectedOptions,
+        [selectedTab]: firstOption,
+      })
+    }
+  }, [selectedTab, selectedUsage])
+
   return (
     <section>
       <SectionHeader header="Usage" scope={scope} tag={header} />
@@ -45,10 +72,7 @@ export const DocUsage: FunctionComponent<Props> = ({
       <ul class={styles.options}>
         {usageTabs.map((usageTab) => {
           const usageItem = usage[usageTab]
-
-          if (!usageItem) {
-            return null
-          }
+          if (!usageItem) return null
 
           return (
             <li class={styles.option} key={usageTab}>
@@ -67,13 +91,74 @@ export const DocUsage: FunctionComponent<Props> = ({
                 }}
               >
                 {usageItem.title}
+
+                {'options' in usageItem && (
+                  <select
+                    class={styles.optionSelect}
+                    onChange={(e) => {
+                      setSelectedOptions({
+                        ...selectedOptions,
+                        [usageTab]: (e.target as HTMLInputElement).value,
+                      })
+                    }}
+                    value={
+                      selectedOptions[usageTab] ||
+                      Object.values(usageItem.options)[0]
+                    }
+                  >
+                    {Object.entries(usageItem.options).map(([title, value]) => (
+                      <option value={value} key={value}>
+                        {title}
+                      </option>
+                    ))}
+                  </select>
+                )}
               </a>
             </li>
           )
         })}
       </ul>
 
-      <Content code={selectedUsage.code} text={selectedUsage.text} />
+      <Content
+        code={
+          'options' in selectedUsage
+            ? selectedUsage.code(
+                selectedOptions[selectedTab] ||
+                  Object.values(selectedUsage.options)[0]
+              )
+            : selectedUsage.code
+        }
+        text={selectedUsage.text}
+      />
     </section>
   )
 }
+
+function getSelectedOptions() {
+  try {
+    return JSON.parse(
+      window.localStorage.getItem(LS_SELECTED_OPTIONS_KEY) || '{}'
+    )
+  } catch (_e) {
+    return {}
+  }
+}
+
+interface ContentProps {
+  code: string
+  text?: string
+}
+
+export const Content: FunctionComponent<ContentProps> = ({ code, text }) => (
+  <div>
+    <div>
+      <Code value={code} />
+    </div>
+
+    {text && (
+      <div>
+        <Markdown value={text} />
+      </div>
+    )}
+  </div>
+)
