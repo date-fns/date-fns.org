bf32ca19073b518581b41fab21f4477de4524de7
Samiullah Akram <samiullah.akram.3009@gmail.com>
2026-08-16 21:34:22 +0500

    Add cancel button to subscription banner

--- src/ui/components/JobsSubscribeWidget/index.tsx
+++ src/ui/components/JobsSubscribeWidget/index.tsx
@@ -1,48 +1,64 @@
 import { h } from "preact";
+import { useState } from "preact/hooks";
 import * as styles from "./styles.css";
 // import twitterIconURL from './img/twitterBlack.svg'
 
-export const JobsSubscribeWidget = () => (
-  <div class={styles.container}>
-    <div class={styles.block}>
-      <div class={styles.blockContent}>
-        <div class={styles.header}>
-          Get awesome JavaScript jobs in your email
+export const JobsSubscribeWidget = () => {
+  const [dismissed, setDismissed] = useState(false);
+
+  if (dismissed) return null;
+
+  return (
+    <div class={styles.container}>
+      <button
+        class={styles.closeButton}
+        type="button"
+        aria-label="Dismiss"
+        onClick={() => setDismissed(true)}
+      >
+        ×
+      </button>
+
+      <div class={styles.block}>
+        <div class={styles.blockContent}>
+          <div class={styles.header}>
+            Get awesome JavaScript jobs in your email
+          </div>
+
+          <form
+            class={styles.form}
+            action="https://cafe.us16.list-manage.com/subscribe/post?u=bf10ea6555c1dc38f3bcfa7e5&amp;id=b7d6fa4833"
+            method="post"
+            target="_blank"
+            rel="noreferrer,noopener"
+          >
+            <input
+              class={styles.input}
+              name="EMAIL"
+              placeholder="Email"
+              type="email"
+              required
+            />
+
+            <button class={styles.subscribeButton} type="submit">
+              Subscribe
+            </button>
+          </form>
         </div>
-
-        <form
-          class={styles.form}
-          action="https://cafe.us16.list-manage.com/subscribe/post?u=bf10ea6555c1dc38f3bcfa7e5&amp;id=b7d6fa4833"
-          method="post"
-          target="_blank"
-          rel="noreferrer,noopener"
-        >
-          <input
-            class={styles.input}
-            name="EMAIL"
-            placeholder="Email"
-            type="email"
-            required
-          />
-
-          <button class={styles.subscribeButton} type="submit">
-            Subscribe
-          </button>
-        </form>
       </div>
-    </div>
 
-    <div class={styles.footer}>
-      Subscribe to support date-fns development, it's free!
-    </div>
+      <div class={styles.footer}>
+        Subscribe to support date-fns development, it's free!
+      </div>
 
-    {/* <a
-      class={styles.twitterLink}
-      href="https://twitter.com/date_fns"
-      target="_blank"
-    >
-      <img class={styles.twitterIcon} src={twitterIconURL} alt="Twitter icon" />
-      Follow on Twitter
-    </a> */}
-  </div>
-);
+      {/* 
+        class={styles.twitterLink}
+        href="https://twitter.com/date_fns"
+        target="_blank"
+      >
+        <img class={styles.twitterIcon} src={twitterIconURL} alt="Twitter icon" />
+        Follow on Twitter
+      </a> */}
+    </div>
+  );
+};
